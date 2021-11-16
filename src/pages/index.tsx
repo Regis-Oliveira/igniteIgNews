d15b3ec3830570ea09b437f0e0bcce5cbb3f06d7
regis-oliveira <regis_crysis@hotmail.com>
2021-11-16 10:42:37 -0300

    feature: create home page and header

---
+++ src/pages/index.tsx
@@ -0,0 +1,33 @@
+import Head from 'next/head';
+import Image from 'next/image';
+
+import styles from './home.module.scss';
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Home | ig.news</title>
+      </Head>
+      
+      <main className={styles.contentContainer}>
+        <section className={styles.hero}>
+          <span>👏 Hey, welcome</span>
+          <h1>New about <br /> the <span>React</span> world.</h1>
+
+          <p>
+            Get access to all the publications <br />
+            <span>for $9.90 month</span>
+          </p>
+        </section>
+
+        <Image 
+          src="/images/avatar.svg" 
+          alt="Girl coding"
+          width={500}
+          height={500}
+        />
+      </main>
+    </>
+  )
+}

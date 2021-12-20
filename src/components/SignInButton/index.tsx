f850fd6e421f36788ca237e61388a98e9827094c
regis-oliveira <regis_crysis@hotmail.com>
2021-12-20 03:49:49 -0300

    feat: config stripe connection, config faunaDb and serverless function

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,27 +1,34 @@
 import { FaGithub } from 'react-icons/fa';
 import { FiX } from 'react-icons/fi';
+import { signIn, signOut, useSession } from 'next-auth/react';
 
 import styles from './styles.module.scss';
 
 export function SignInButton() {
-  const isUserLoggedIn = true;
+  const { data: session } = useSession();
 
-  return isUserLoggedIn ? (
+  if (session) {
+    return (
+      <button
+        className={styles.signInButton} 
+        type="button"
+        onClick={() => signOut()}
+      >
+        <FaGithub color="#04d361" />
+        {session.user.name} 
+        <FiX className={styles.closeIcon} color="#737380" />
+      </button>
+    )
+  }
+
+  return (
     <button
       className={styles.signInButton} 
       type="button"
-    >
-      <FaGithub color="#04d361" />
-      Regis de Oliveira 
-      <FiX className={styles.closeIcon} color="#737380" />
-    </button>
-  ): (
-    <button
-      className={styles.signInButton} 
-      type="button"
+      onClick={() => signIn('github')}
     >
       <FaGithub color="#eba417" />
       Sign In with Github
     </button>
-  )
+  );
 }
